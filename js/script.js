ede1740c6a7dfc667b86df98350fe90e60c1dbf1
Flead <flead@yandex.ru>
2017-10-19 22:30:55 +0000

    fin

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    controlsContainer: $(".custom-controls-container"),
+    customDirectionNav: $(".custom-navigation a")
+  });
+  $('.flex-control-nav li a').text('');
+  $('.photos>div').click(function() {
+    var name = $(this).find('.image').attr('alt');
+    $('.show-fon').show('slow');
+    $('html, body').css('overflow', 'hidden');
+    $('.show-fon').append('<img src="photos/'+name+'-pr.jpg" class="image-lg">');
+    console.log(name);
+  });
+  $('.show-fon').on('click', function() {
+    $('.image-lg').remove();
+    $(this).hide();
+    $('html, body').css('overflow', 'auto');
+  });
+  $('.hover-bg').append('<i class="fa fa-plus" aria-hidden="true"></i>');
+  $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+  $('.logo-footer').on('click', function(event) {
+    event.preventDefault();
+    var el = $(this).attr('href');
+    $('body,html').animate({scrollTop: $(el).offset().top}, 1000);
+  });
+  $('.nav-click').on('click', 'a', function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body, html').animate({scrollTop: top}, 1500);
+  });
+  var waypoint1 = new Waypoint({
+    element: $('#home'),
+    handler: function() {
+      $('.nav-click>ul>li>a').css('background-image', 'url(img/nav-click.png)');
+      $('.nav-click>ul>li:nth-child(1)>a').css('background-image', 'url(img/nav-click-active.png)');
+    },
+    offset:-20
+  });
+  var waypoint2 = new Waypoint({
+    element: $('#about'),
+    handler: function() {
+      $('.nav-click>ul>li>a').css('background-image', 'url(img/nav-click.png)');
+      $('.nav-click>ul>li:nth-child(2)>a').css('background-image', 'url(img/nav-click-active.png)');
+    }
+  });
+  var waypoint3 = new Waypoint({
+    element: $('#works'),
+    handler: function() {
+      $('.nav-click>ul>li>a').css('background-image', 'url(img/nav-click.png)');
+      $('.nav-click>ul>li:nth-child(3)>a').css('background-image', 'url(img/nav-click-active.png)');
+    }
+  });
+  var waypoint4 = new Waypoint({
+    element: $('#contact'),
+    handler: function() {
+      $('.nav-click>ul>li>a').css('background-image', 'url(img/nav-click.png)');
+      $('.nav-click>ul>li:nth-child(4)>a').css('background-image', 'url(img/nav-click-active.png)');
+    },
+    offset:100
+  });
+});
